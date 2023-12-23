f549409ead90db0ce2e8cdd91b28957af5298904
naruse666 <become15@i.softbank.jp>
2023-12-23 11:59:07 +0900

    add atlantis deploy sample

---
+++ atlantis-server/main.tf
@@ -0,0 +1,2 @@
+# Deployment Docs: https://www.runatlantis.io/docs/deployment.html
+# Here is ECS on Fargate terraform module: https://registry.terraform.io/modules/terraform-aws-modules/atlantis/aws/latest
